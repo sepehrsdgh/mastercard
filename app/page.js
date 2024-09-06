885f8707f4c0dd181c6b16edb7c4d488d2ca172d
a-meraji <merajiamin1997@gmail.com>
2024-09-06 13:18:39 +0330

    scrolling bug fixed

--- app/page.js
+++ app/page.js
@@ -9,16 +9,15 @@ import ShiningButton from "@/components/landing/shiningButton";
 // If loading a variable font, you don't need to specify the font weight
 const plus = Plus_Jakarta_Sans({ subsets: ["latin"] });
 export default function Home() {
-  
   return (
-    <main className="max-h-screen bg-[#0A0A0A] relative">
+    <main  className="h-[96vh] relative">
       {/* ellipese usage => just for graphic design */}
       <EllipsPurple />
       <EllipseBlue />
       {/* ellipese usage => just for graphic design */}
-      <div className="h-screen grid grid-rows-2">
+      <div className="h-[97vh] grid grid-rows-2">
         {/* grid row1 */}
-        <div className="relative ">
+        <div className="relative mb-3">
           <div className="absolute bottom-0">
             <MasterCardLighSVG />
           </div>
@@ -28,14 +27,17 @@ export default function Home() {
         </div>
         {/* grid row 2 */}
       <div className={plus.className}>
-        <div className="text-[2.2em] text-white absolute top-[63vh] px-[12vw]">
+        <div className="text-[2.2em] text-white absolute top-[56vh] px-[9vw]">
           Mastercard Linked to the Crypto Network!
         </div>
       </div>
       </div>
-      <div className="absolute no-scrollbar overflow-y-hidden  bottom-5 left-0 right-0 flex justify-center items-center w-full">
+      <div className={plus.className}>
+
+      <div className="absolute no-scrollbar overflow-y-hidden  bottom-0 mb-20 left-0 right-0 flex justify-center items-center w-full">
         <ShiningButton buttonText={"Let's go!"} />
-      </div>
+        </div>
+        </div>
     </main>
   );
 }
--- components/landing/Ellipeses.jsx
+++ components/landing/Ellipeses.jsx
@@ -9,7 +9,7 @@ function EllipsPurple() {
         background:
           " radial-gradient(circle, rgba(55,27,72,1) 0%, rgba(55,27,72,0) 100%)",
       }}
-      className="absolute right-[6%] top-[10vh] w-[75vw] h-[35vh]  rounded-full blur-[2rem]"></div>
+      className="absolute right-[6%] top-[7vh] w-[75vw] h-[35vh]  rounded-full blur-[2rem]"></div>
     </div>
   );
 }
@@ -22,7 +22,7 @@ function EllipseBlue() {
           background:
             " radial-gradient(circle, #221B48 0%, #221B4800 100%)",
         }}
-         className="absolute left-[10%] top-[30vh] w-[60vw] h-[30vh] rounded-full blur-[3rem]"></div>
+         className="absolute left-[10%] top-[27vh] w-[60vw] h-[30vh] rounded-full blur-[3rem]"></div>
       </div>
     );
   }
